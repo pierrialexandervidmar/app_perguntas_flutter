82e751d92d98082662e6215f2be2b44823535fec
pierrialexandervidmar <pierri@hotmail.com.br>
2024-03-03 21:53:34 -0300

    Ressubindo o projeto

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+main() {
+  return runApp(PerguntaApp());
+}
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+
+  void _responder() {
+    setState(() {
+      _perguntaSelecionada++;      
+    });
+
+    print(_perguntaSelecionada);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[_perguntaSelecionada]),
+            ElevatedButton(
+              onPressed: _responder,
+              child: Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: _responder,
+              child: Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: _responder,
+              child: Text('Resposta 3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  @override
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
+  }
+}
